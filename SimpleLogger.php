9d35b0b8f7d3aaf0a07adea55f09746e77bd55b9
Patrice Gauthier <patgauth@gmail.com>
2013-09-13 14:50:15 -0400

    Version 2 - Using new Logger interface

--- SimpleLogger.php
+++ SimpleLogger.php
@@ -1,6 +1,67 @@
 <?php
 
-class SimpleLogger {
+class Logger
+{
+    /**
+     * @var LoggerHandler
+     */
+    private static $loggerHandler;
+
+    public static function setLoggerHandler(LoggerHandler $logger)
+    {
+        self::$loggerHandler = $logger;
+    }
+
+    public static function debug($message)
+    {
+        self::$loggerHandler->debug($message);
+    }
+
+    public static function info($message)
+    {
+        self::$loggerHandler->info($message);
+    }
+
+    public static function warn($message)
+    {
+        self::$loggerHandler->warn($message);
+    }
+
+    public static function error($message)
+    {
+        self::$loggerHandler->error($message);
+    }
+
+    public static function log($levelName, $message)
+    {
+        self::$loggerHandler->log($levelName, $message);
+    }
+
+    public static function getInstance()
+    {
+        return self::$loggerHandler;
+    }
+}
+
+interface LoggerHandler
+{
+    public function __construct($logDirectory, $logFileName, $configFilePath, $consoleOverride);
+
+    public function debug($message);
+
+    public function info($message);
+
+    public function warn($message);
+
+    public function error($message);
+
+    public function log($levelName, $message);
+
+    public function locateConfigFile($filePath);
+}
+
+class SimpleLogger implements LoggerHandler
+{
     const DEBUG = 1;
     const INFO = 2;
     const WARN = 3;
@@ -17,21 +78,32 @@ class SimpleLogger {
         'WARN' => SimpleLogger::WARN,
         'ERROR' => SimpleLogger::ERROR
     );
-
     private $file;
     private $logDirectory;
     private $logFilename;
     private $dateFormat = "H:i:s d/m/y";
     private $config;
+    /**
+     * From a config file
+     * ex:
+     * [file]
+     * level = debug
+     * [console]
+     * level = error
+     * note: console output is sent by email when it's running as cron
+     */
     private $levelsFromConfig;
 
-    function __construct($logDirectory, $logFileName, $configFilePath = null, $consoleOverride = false, $cleanOnOpen = false) {
+    public function __construct($logDirectory, $logFileName, $configFilePath = null, $consoleOverride = false)
+    {
         date_default_timezone_set("America/Montreal");
         $this->logDirectory = $logDirectory;
         $this->logFilename = $logFileName;
         $logFilePath = $logDirectory . '/' . $logFileName;
 
+        $this->assertDirectoryExists($logDirectory);
         $this->rotateLog();
+
         if (!$configFilePath) {
             $this->locateConfigFile("logger.ini");
         } else {
@@ -41,11 +113,7 @@ class SimpleLogger {
         if ($consoleOverride) {
             $this->levelsFromConfig['console'] = $this->levels[strtoupper($consoleOverride)];
         }
-        if (!$cleanOnOpen) {
-            $this->file = fopen($logFilePath, 'a');
-        } else {
-            $this->file = fopen($logFilePath, 'w');
-        }
+        $this->file = fopen($logFilePath, 'a');
         if (!$this->file) {
             echo "Check your main php error log\n";
             $message = sprintf("Error while trying to write to log file %s", $logFilePath);
@@ -56,11 +124,13 @@ class SimpleLogger {
         self::$instance = $this;
     }
 
-    public static function getInstance() {
+    public static function getInstance()
+    {
         return self::$instance;
     }
 
-    public function rotateLog() {
+    private function rotateLog()
+    {
         $logFilePath = $this->logDirectory . '/' . $this->logFilename;
         $today = date('d');
         $timestamp = @filemtime($this->logDirectory . '/' . $this->logFilename);
@@ -69,48 +139,65 @@ class SimpleLogger {
             $lastOpened = date('d', $timestamp);
             $archivesDirectoryFullPath = $this->logDirectory . '/' . SimpleLogger::ARCHIVESDIR;
 
-            $archivesDir = @dir($archivesDirectoryFullPath);
-            if (!$archivesDir) {
-                mkdir($archivesDirectoryFullPath);
-            }
+            $this->assertDirectoryExists($archivesDirectoryFullPath);
 
             if (strcmp($today, $lastOpened) != 0) {
-                $logFileNameWithDate = substr($this->logFilename, 0, strpos($this->logFilename, '.')) . '-' . date('y-m-d', $timestamp) . '.log';
+                $logFileNameWithDate =
+                    substr($this->logFilename, 0, strpos($this->logFilename, '.'))
+                    . '-' .
+                    date('y-m-d', $timestamp) . '.log';
                 exec(sprintf("mv %s %s\n", $logFilePath, $archivesDirectoryFullPath . '/' . $logFileNameWithDate));
             }
         }
     }
 
-    public function debug($message) {
+    private function assertDirectoryExists($dirAbsolute)
+    {
+        $foundDir = @dir($dirAbsolute);
+        if (!$foundDir) {
+            mkdir($dirAbsolute);
+        }
+    }
+
+    public function debug($message)
+    {
         $this->log("DEBUG", $message);
     }
 
-    public function info($message) {
+    public function info($message)
+    {
         $this->log("INFO", $message);
     }
 
-    public function warn($message) {
+    public function warn($message)
+    {
         $this->log("WARN", $message);
     }
 
-    public function error($message) {
+    public function error($message)
+    {
         $this->log("ERROR", $message);
     }
 
-    public function log($levelName, $message) {
+    public function log($levelName, $message)
+    {
         $levelNumber = $this->levels[$levelName];
         $line = sprintf("[%s] %s: %s\n", date($this->dateFormat), $levelName, $message);
-        if ($levelNumber >= $this->levelsFromConfig[SimpleLogger::CONSOLEOUTPUT])
+        if ($levelNumber >= $this->levelsFromConfig[SimpleLogger::CONSOLEOUTPUT]) {
             echo $line;
-        if ($levelNumber >= $this->levelsFromConfig[SimpleLogger::FILEOUTPUT])
+        }
+        if ($levelNumber >= $this->levelsFromConfig[SimpleLogger::FILEOUTPUT]) {
             fwrite($this->file, $line);
+        }
     }
 
-    function __destruct() {
+    public function __destruct()
+    {
         fclose($this->file);
     }
 
-    public function locateConfigFile($filePath) {
+    public function locateConfigFile($filePath)
+    {
         $this->config = parse_ini_file($filePath, true);
         if (!$this->config) {
             $message = "Invalid config file and path, check it";
@@ -118,10 +205,87 @@ class SimpleLogger {
         }
     }
 
-    private function setLevelForOutput() {
+    private function setLevelForOutput()
+    {
         $this->levelsFromConfig = array();
         foreach ($this->config as $outputName => $output) {
             $this->levelsFromConfig[$outputName] = $this->levels[strtoupper($output['level'])];
         }
     }
 }
+
+class FakeLogger implements LoggerHandler
+{
+
+    private static $instance;
+
+    public static function getInstance()
+    {
+        return self::$instance;
+    }
+
+    public function __construct($logDirectory = null, $logFileName = null, $configFilePath = null,
+        $consoleOverride = null)
+    {
+        self::$instance = $this;
+    }
+
+    public function debug($message)
+    {
+    }
+
+    public function info($message)
+    {
+    }
+
+    public function warn($message)
+    {
+    }
+
+    public function error($message)
+    {
+    }
+
+    public function log($levelName, $message)
+    {
+        echo $levelName, $message;
+    }
+
+    public function locateConfigFile($filePath)
+    {
+    }
+}
+
+function errorHandler($errno, $errstr, $errfile, $errline)
+{
+    if (!is_null(Logger::getInstance())) {
+        switch ($errno) {
+            case E_NOTICE:
+            case E_USER_NOTICE:
+            case E_DEPRECATED:
+            case E_USER_DEPRECATED:
+            case E_STRICT:
+                Logger::info(sprintf("Notice: %s on line %s : %s", $errfile, $errline, $errstr));
+                break;
+
+            case E_WARNING:
+            case E_USER_WARNING:
+                Logger::warn(sprintf("File: %s on line %s : %s", $errfile, $errline, $errstr));
+                break;
+
+            case E_ERROR:
+            case E_USER_ERROR:
+                Logger::error(sprintf("File: %s on line %s : %s", $errfile, $errline, $errstr));
+                exit();
+
+            default:
+                Logger::error(sprintf("Unknown error : %s on line %s", $errfile, $errline));
+                exit();
+        }
+    } else {
+        trigger_error("No SimpleLogger instance");
+    }
+}
+
+set_error_handler("errorHandler");
+
